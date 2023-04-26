8683352d8b7c0e05f4a0cc0d95d493465716dd68
TurquoisePenguin7 <turquoisehairedpenguinhorn@gmail.com>
2023-04-27 01:40:07 +0300

    added exception handling

--- metadata_date_extract/main.py
+++ metadata_date_extract/main.py
@@ -10,17 +10,26 @@ def main():
     directory = Path.cwd() / "processed_files"
     directory.mkdir(exist_ok=True)
 
-    def to_destination_dir(file, path=None):
+
+    def file_to_destination_dir(file):
         date = dt.datetime.strptime(time.ctime(os.path.getctime(file)), "%a %b %d %H:%M:%S %Y")
         output_file_name = f"{date.year}{date.month}{date.day}{date.hour}{date.minute}_{file.stem}{file.suffix}"
         file_location = directory / output_file_name
         shutil.copyfile(file, file_location)
+
+    
     
     # test case for images...
-
-    images = Path.cwd() / "images"
-    for i in images.glob('*.*'):
-        to_destination_dir(i)
+    try:
+        # path specification
+        images = Path(sys.argv[1])
+        for i in images.glob(f'*.*'):
+            file_to_destination_dir(i)
+    except IndexError:
+        print("Please, specify the file name and/or extension.")
+        sys.exit()
+    except FileNotFoundError:
+        print("File doesn't exist...")
 
 if __name__ == "__main__":
     main()
