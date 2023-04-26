2bb27cd41ec546d6e42b473d8e0d8255ce6de29a
TurquoisePenguin7 <turquoisehairedpenguinhorn@gmail.com>
2023-04-26 14:50:59 +0300

    quick fix

--- metadata_date_extract/main.py
+++ metadata_date_extract/main.py
@@ -12,10 +12,12 @@ def main():
 
     def to_destination_dir(file, path=None):
         date = dt.datetime.strptime(time.ctime(os.path.getctime(file)), "%a %b %d %H:%M:%S %Y")
-        output_file_name = f"{date.month}_{date.day}_{date.year}_{file.stem}{file.suffix}"
+        output_file_name = f"{date.year}{date.month}{date.day}{date.hour}{date.minute}_{file.stem}{file.suffix}"
         file_location = directory / output_file_name
         shutil.copyfile(file, file_location)
     
+    # test case for images...
+
     images = Path.cwd() / "images"
     for i in images.glob('*.*'):
         to_destination_dir(i)
